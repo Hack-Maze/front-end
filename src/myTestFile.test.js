7b355fb13df26a8bf0b69a20df92a3f24a4430bb
mahmoud-abo-al-ela <mahmoud.aboalela2786@gmail.com>
2023-11-25 19:57:53 +0200

    edit test file

--- src/myTestFile.test.js
+++ src/myTestFile.test.js
@@ -1,12 +1,10 @@
-// myTestFile.test.js
-
 // eslint-disable-next-line no-undef
 test("Example test using mock function", () => {
-  // You can use the mockFunction here
   // eslint-disable-next-line no-undef
   global.mockFunction.mockReturnValue("Mocked value");
 
-  // Your test assertions using the mocked function
   // eslint-disable-next-line no-undef
   expect(global.mockFunction()).toBe("Mocked value");
+
+  console.log("Hello, World!");
 });
