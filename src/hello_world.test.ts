9e65750598ffcba2e3080620db7156c5fc71b8a6
Mostafa-ewida <71707534+Mostafa-ewida@users.noreply.github.com>
2023-11-27 18:14:25 +0200

    Create hello_world.test.ts

---
+++ src/hello_world.test.ts
@@ -0,0 +1,16 @@
+import { hello } from "../src/hello_world";
+
+describe("test hello", () => {
+  it("should return hello world", () => {
+    expect(hello()).toBe("Hello world!");
+  });
+});
+
+const greeting = 'world';
+
+export function hello(world: string = greeting): string {
+    if (world == "Bob") {
+        return "Hiya Bob!";
+    }
+    return `Hello ${world}!`;
+}
