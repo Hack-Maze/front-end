497337aa4a446a7c058669494d981e8738f284a2
Mahmoud Ali <79024928+mahmoud-abo-al-ela@users.noreply.github.com>
2024-03-07 17:07:09 +0000

    update env

--- utils/CustomFetsh.jsx
+++ utils/CustomFetsh.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
+const apiUrl = `https://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/api/v1`;
+// const apiUrl = `${import.meta.env.VITE_API_URL}api/v1`;
+console.log(apiUrl);
 const customFetch = axios.create({
-  baseURL: `${apiUrl}/api/v1/`,
+  baseURL: `${apiUrl}/`,
 });
 
 export default customFetch;
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,15 @@ import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 
 dotenv.config();
+// const apiUrl = `${process.env.VITE_API_URL}`;
+const apiUrl = `https://${process.env.VITE_BACKEND_HOST}:${process.env.VITE_BACKEND_PORT}`;
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_REACT_APP_API_URL,
+        target: apiUrl,
       },
     },
   },
