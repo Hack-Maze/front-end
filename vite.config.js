80f5631eece47860f162dfab3e3c546097a72ce7
Mahmoud Ali <79024928+mahmoud-abo-al-ela@users.noreply.github.com>
2024-03-09 17:17:48 +0000

    edit env

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config();
-const apiUrl = `${process.env.VITE_API_URL}`;
-// const apiUrl = `https://${process.env.VITE_BACKEND_HOST}:${process.env.VITE_BACKEND_PORT}`;
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: apiUrl,
+        target: import.meta.env.VITE_API_URL,
       },
     },
   },
