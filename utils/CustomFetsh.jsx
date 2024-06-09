86ca8394edd37748bf5d1bca07d9101f35305df4
mahmoud-abo-al-ela <mahmoud.aboalela2786@gmail.com>
2024-06-09 16:53:14 +0300

    improvements

--- utils/CustomFetsh.jsx
+++ utils/CustomFetsh.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const apiUrl = `${window.VITE_API_URL}api/v1`;
-const apiUrl = `${import.meta.env.VITE_API_URL}api/v1`;
+const apiUrl = `${window.VITE_API_URL}api/v1`;
+// const apiUrl = `${import.meta.env.VITE_API_URL}api/v1`;
 
 console.log(apiUrl);
 const customFetch = axios.create({
